afb31928a3ae5a1727ed3ed29a2bea0863b6c9d5
Mpho Mphego <mpho112@gmail.com>
2019-04-07 14:22:26 +0200

    Updated medium_speech/__init__.py setup.py
    Signed-off-by: Mpho Mphego <mpho112@gmail.com>

--- medium_speech/__init__.py
+++ medium_speech/__init__.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3
+"""# -*- coding: utf-8 -*-"""
 
+import re
 import subprocess
+from contextlib import suppress
 from io import StringIO
 from pathlib import Path
 
 import docker
 from gtts import gTTS
 from markdown import Markdown
-from shutil import rmtree
+
+
+def remove_tags(text):
+    """Remove HTML tags from string
+
+    Args:
+        text (String):
+
+    Returns:
+        String:
+    """
+    TAG_RE = re.compile(r"<[^>]+>")
+    return TAG_RE.sub("", text)
 
 
 def which(program):
     """Takes a program name or full path,
     and returns the full path of the requested executable.
+
+    Args:
+        program (str): Unix program
+
+    Returns:
+        String: path to program
     """
     return subprocess.check_output(["which", program]).strip().decode()
 
@@ -30,57 +51,143 @@ def unmark_element(element, stream=None):
     return stream.getvalue()
 
 
-def md_to_text(text=None, filename=None, tab_length=4):
+def bytes_to_str(text=None):
+    """Decode bytes to string
+
+    Args:
+        text (None, optional): bytes
+
+    Returns:
+        text: str
+
+    Example:
+        >>> text = b"Hello World"
+        >>> print(bytes_to_str(text))
+            "Hello World"
+    """
+    if isinstance(text, bytes):
+        text = text.decode("UTF-8").strip()
+        return text
+
+
+def splits_words(words=None, char_length=99):
+    """Split list of words to n-characters
+
+    Args:
+        words (List, optional): list of words
+        char_length (int, optional): character length to split to!
+
+    Returns:
+        TYPE: Description
+
+    Example:
+        >>> text = ["<a>", "hello", "World", "!", "</a>"]
+        >>> splits_words(text)
+            [['', 'hello', 'World', '!', '']]
+I    """
+    if isinstance(words, list):
+        words = [remove_tags(i) for i in words]
+        return [words[n : n + char_length] for n in range(0, len(words), char_length)]
+
+
+def md_to_text(md_text=None, tab_length=4):
+    """Summary
+
+    Args:
+        md_text (None, optional): Description
+        tab_length (int, optional): Description
+
+    Returns:
+        TYPE: Description
+    """
+    text = bytes_to_str(md_text)
     Markdown.output_formats["plain"] = unmark_element
     md = Markdown(output_format="plain")
     md.stripTopLevelTags = False
     md.tab_length = tab_length
-    return md.convert(text)
+    plain_text = md.convert(text)
+    return [x for x in plain_text.split("\n") if x]
 
 
 def read_from_medium(
-    docker_container="mmphego/mediumexporter",
     medium_url=None,
+    docker_container="mmphego/mediumexporter",
     runonce=True,
-    filename="medium.md",
+    save_to_file=False,
 ):
-    client = docker.from_env()
-    with open(filename, "wb") as _f:
-        data = client.containers.run(docker_container, medium_url, remove=runonce)
-        _f.write(data)
-    return data
+    """Summary
+
+    Args:
+        medium_url (None, optional): Description
+        docker_container (str, optional): Description
+        runonce (bool, optional): Description
+        save_to_file (bool, optional): Description
+    """
+    if medium_url:
+        try:
+            client = docker.from_env()
+            data = client.containers.run(docker_container, medium_url, remove=runonce)
+        except Exception as _err:
+            raise RuntimeError(f"{_err}: Failed to retrieve Medium post.")
+        if save_to_file:
+            with open("medium.md", "wb") as _f:
+                _f.write(data)
+        return data
 
 
 def read_from_file(filename):
-    with open(filename) as _f:
-        lines = [x for x in [x.rstrip() for x in _f.readlines()] if x]
-    return lines
+    """Read Markdown file
+
+    Args:
+        filename (str): Markdown file
+
+    Returns:
+        bytes: Contents of file
+    """
+    with open(filename, "rb") as _f:
+        return _f.read()
 
 
-def get_markdown_titles(lines):
-    return {i: "" for i in lines if "# " in i}
+def text_to_speech(text_from_markdown=[], tmp_dir="/tmp"):
+    """Generate speech from text using Google TTS API
+
+    Args:
+        text_from_markdown (list, optional): Markdown text
+        tmp_dir (str, "/tmp"): Directory to save the mp3 files
+    """
+    if isinstance(text_from_markdown, list):
+        splitted_words = splits_words(text_from_markdown)
+        for words in splitted_words:
+            for count, line in enumerate(words, 1):
+                print(len(line), ":", line)
+                if line:
+                    tts = gTTS(text=line, lang="en-us")
+                    tts.save(f"{tmp_dir}/{count}.mp3")
 
 
-def text_to_speech(words, tmp_dir="/tmp", file_format="mp3"):
-    for count, line in enumerate(words, 1):
-        tts = gTTS(text=line, lang="en-us")
-        tts.save(f"{tmp_dir}/{count}.{file_format}")
+def play_it(play_with="mpg123", tmp_dir="/tmp", file_format="mp3", delete_file=True):
+    """Play mp3 files
 
-
-def play_it(play_with="mpg123", tmp_dir="/tmp", file_format="mp3"):
+    Args:
+        play_with (str, "mph123"): Unix/Linux program to play mp3 with!
+        tmp_dir (str, "/tmp"): Directory where mp3 files are saved!
+        file_format (str, "mp3"): file format
+        delete_file (bool, True): Delete file when done!
+    """
     FNULL = open(subprocess.os.devnull, "wb")
     play_with = which(play_with)
     tmp_dir = Path(tmp_dir)
     mp3_files = tmp_dir.glob(f"*{file_format}")
-    try:
-        for mp3_file in sorted(mp3_files):
+    for mp3_file in sorted(mp3_files):
+        if mp3_file.is_file():
             subprocess.call(
                 f"{play_with} {mp3_file}",
                 shell=True,
                 stdout=FNULL,
                 stderr=subprocess.STDOUT,
             )
-    finally:
-        # rmtree(os.path.join(here, "dist"))
-        # _ = [subprocess.call(f"rm -rf {mp3_file}") for mp3_file in sorted(mp3_files)]
-        pass
+        if delete_file:
+            with suppress(FileNotFoundError):
+                mp3_file.unlink()
+
+
